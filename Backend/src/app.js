8d6c9abcf3d3b309ec9e2634c0d702bb8293b619
shekhar <shekhart442@gmail.com>
2025-02-20 20:36:41 +0530

    backend completed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const aiRoutes = require('../src/routes/ai.routes');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello from BACKEND!');
 })
+
+app.use('/ai', aiRoutes)
 
 module.exports = app;
